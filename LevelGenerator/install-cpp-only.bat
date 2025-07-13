d1be952471abf27207931e0fced9a2e8366aee4e
ArthurGW <agordonwright@gmail.com>
2025-07-13 21:16:48 +1200

    Add cpp-only script

---
+++ LevelGenerator/install-cpp-only.bat
@@ -0,0 +1,7 @@
+@call "C:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvars64.bat"
+
+%CMAKE_EXE% -DCMAKE_MAKE_PROGRAM=%NINJA_EXE% -G "Ninja Multi-Config" -S . -B ninja_build
+
+%CMAKE_EXE% --build ninja_build --config Release --target level-gen-cpp
+
+%CMAKE_EXE% --install ninja_build --config Release --prefix ../Assets --component level-gen
