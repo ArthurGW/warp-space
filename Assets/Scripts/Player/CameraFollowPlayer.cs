d79a12d2e22ef585e9001a56eed981227c0049c7
ArthurGW <agordonwright@gmail.com>
2025-08-09 23:10:21 +1200

    Add vertical camera movement

--- Assets/Scripts/Player/CameraFollowPlayer.cs
+++ Assets/Scripts/Player/CameraFollowPlayer.cs
@@ -9,15 +9,19 @@ namespace Player
         private Transform _mainCameraTransform;
         private Transform _playerTransform;
 
-        private InputAction _zoom;
+        private InputAction _zoomAction;
+        private InputAction _lookAction;
 
         public float offset = 50f;
         public float zoomSpeed = 4f;
+        public float lookAngle = 60f;
+        public float lookSpeed = 4f;
 
         private void Awake()
         {
             if (!InputSystem.actions) return;
-            _zoom = InputSystem.actions.FindAction("Player/Zoom");
+            _zoomAction = InputSystem.actions.FindAction("Player/Zoom");
+            _lookAction = InputSystem.actions.FindAction("Player/Look");
         }
 
         private void Start()
@@ -30,19 +34,27 @@ namespace Player
 
         private void Update()
         {
-            var zoom = _zoom.ReadValueAsObject();
-            if (zoom is float amount)
+            if (_zoomAction.ReadValueAsObject() is float amount)
             {
-                offset = Mathf.Clamp(offset + amount * zoomSpeed, 5, 250);
+                offset = Mathf.Clamp(offset + amount * zoomSpeed * Time.deltaTime, 5, 250);
+            }
+            
+            if (_lookAction.ReadValueAsObject() is Vector2 look)
+            {
+                lookAngle = Mathf.Clamp(lookAngle + look.y * lookSpeed * Time.deltaTime, 30, 80);
             }
             Follow();
         }
 
         private void Follow()
         {
-            var offsetDir = _playerTransform.up * 4 - _playerTransform.forward;
-            offsetDir = offsetDir.normalized;
-            _mainCameraTransform.position = _playerTransform.position + offsetDir * offset;
+            // var offsetDir = _playerTransform.up * 4 - _playerTransform.forward;
+            // offsetDir = offsetDir.normalized;
+            var sin = MathF.Sin(lookAngle * MathF.PI/180);
+            var cos = MathF.Cos(lookAngle * MathF.PI/180);
+            var vertical = _playerTransform.up * (offset * sin);
+            var horizontal = -_playerTransform.forward * (offset * cos);
+            _mainCameraTransform.position = _playerTransform.position + vertical + horizontal;
             _mainCameraTransform.LookAt(_playerTransform);
         }
     }
